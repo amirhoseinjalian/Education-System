bdbe3db7be145a095b6b3b46f1f9d0902bc6b5b1
amirhosein.jalian <amirhoseinjalian82@gmail.com>
2023-07-18 09:32:37 +0430

    editing name of methods in StudentQuizRepository

--- src/main/java/com/jalian/maktabfinalproject/repository/StudentQuizRepository.java
+++ src/main/java/com/jalian/maktabfinalproject/repository/StudentQuizRepository.java
@@ -1,9 +1,6 @@
 package com.jalian.maktabfinalproject.repository;
 
-import com.jalian.maktabfinalproject.entity.Answer;
-import com.jalian.maktabfinalproject.entity.Quiz;
-import com.jalian.maktabfinalproject.entity.Student;
-import com.jalian.maktabfinalproject.entity.StudentQuiz;
+import com.jalian.maktabfinalproject.entity.*;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -11,7 +8,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.List;
 
 @RepositoryRestResource
-public interface StudentQuizRepository extends BaseRepository<StudentQuiz, Long> {
+public interface StudentQuizRepository extends BaseRepository<StudentQuiz, StudentQuizKey> {
 
     @Query("update StudentQuiz s set s.isJoined = true where s.student.id = :studentId and s.quiz.id = :quizId")
     void joinedAQuiz(@Param("studentId") String studentId, @Param("quizId") Long quizId);
@@ -19,12 +16,9 @@ public interface StudentQuizRepository extends BaseRepository<StudentQuiz, Long>
     @Query("select s.student from StudentQuiz s where s.quiz.id = :quizId")
     List<Student> getStudentsOfAQuiz(@Param("quizId") Long quizId);
 
-    @Query("select s.student from StudentQuiz s where s.score >= 10")
+    @Query("select s.student from StudentQuiz s where s.grade >= 10")
     List<Student> getPassedStudents(@Param("quizId") Long quizId);
 
-    @Query("select s.student from StudentQuiz s where s.quiz.id = :quizId")
-    List<Student> getStudents(@Param("quizId") Long quizId);
-
     @Query("select s.quiz from StudentQuiz s where s.student.id = :studentId")
-    List<Quiz> getQuizzes(@Param("studentId") String studentId);
+    List<Quiz> getQuizzesOfAStudent(@Param("studentId") String studentId);
 }

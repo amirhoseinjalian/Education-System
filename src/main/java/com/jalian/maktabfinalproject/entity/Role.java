9fa079c51df6ef8b9ff057c1c5ee536ce6429d16
amirhosein.jalian <amirhoseinjalian82@gmail.com>
2023-07-19 10:14:01 +0430

    removing default value from role

--- src/main/java/com/jalian/maktabfinalproject/entity/Role.java
+++ src/main/java/com/jalian/maktabfinalproject/entity/Role.java
@@ -3,6 +3,7 @@ package com.jalian.maktabfinalproject.entity;
 import com.fasterxml.jackson.annotation.*;
 import lombok.*;
 import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 import java.util.List;
@@ -15,6 +16,7 @@ import java.util.List;
 @Builder@NoArgsConstructor
 @AllArgsConstructor
 @DynamicInsert
+@DynamicUpdate
 @JsonIdentityInfo(
         generator = ObjectIdGenerators.PropertyGenerator.class,
         property = "id")
@@ -25,7 +27,7 @@ import java.util.List;
 public class Role extends LongIdEntity {
 
     @Enumerated(EnumType.STRING)
-    @Column(length = 20, columnDefinition = "enum('ADMIN','TEACHER','STUDENT') DEFAULT 'STUDENT'")
+    @Column(length = 20, columnDefinition = "enum('ADMIN','TEACHER','STUDENT')")
     @JsonProperty
     private RoleNames name;
 

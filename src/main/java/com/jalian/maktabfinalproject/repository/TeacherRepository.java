e9b2fd8fe4af1ea5c434071a4c4353e89e10f2a2
amirhosein.jalian <amirhoseinjalian82@gmail.com>
2023-07-28 21:48:04 +0430

    deleting addCourse method

--- src/main/java/com/jalian/maktabfinalproject/repository/TeacherRepository.java
+++ src/main/java/com/jalian/maktabfinalproject/repository/TeacherRepository.java
@@ -6,7 +6,4 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(exported = false)
 public interface TeacherRepository extends PersonRepository<Teacher> {
 
-    //bayad test beshe////////////////////////////////////////////////////////////////
-    /*@Query("update Teacher t set t.courses = t.courses + #{#course}")
-    void addCourse(@Param("course") Course course);*/
 }

3b8baf24b52d0a4aa0b7c50e0274eb7c0e19b81c
amirhosein.jalian <amirhoseinjalian82@gmail.com>
2023-08-02 18:18:37 +0430

    editing files to handle createQuiz method in TeacherController

--- src/main/java/com/jalian/maktabfinalproject/entity/StudentQuizKey.java
+++ src/main/java/com/jalian/maktabfinalproject/entity/StudentQuizKey.java
@@ -11,6 +11,7 @@ import java.io.Serializable;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode//needed!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 @Builder
 public class StudentQuizKey implements Serializable {
 

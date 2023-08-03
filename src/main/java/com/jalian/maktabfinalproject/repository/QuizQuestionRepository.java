b486a27c9fdf46f7852e4ab1683203d07a21630d
amirhosein.jalian <amirhoseinjalian82@gmail.com>
2023-08-03 13:52:10 +0430

    commenting

--- src/main/java/com/jalian/maktabfinalproject/repository/QuizQuestionRepository.java
+++ src/main/java/com/jalian/maktabfinalproject/repository/QuizQuestionRepository.java
@@ -13,18 +13,19 @@ import java.util.List;
 @RepositoryRestResource
 public interface QuizQuestionRepository extends BaseRepository<QuizQuestionJoinTable, QuizQuestionKey> {
 
-    //should be tested!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    @Query("select s.question from QuizQuestionJoinTable s where s.quiz.id = :quizId and Question_TYPE = :questionType")
-    /*@Query("select question from QuizQuestionJoinTable s where s.quiz.id = :quizId and FUNCTION('CLASS', question) = :questionType")*/
-/*
+    /*
     wrong!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     @Query("select s.question from QuizQuestionJoinTable s where s.quiz.id = :quizId and type(s.question) = :questionType")
-*/
-    <Value extends Question> List<Value> getQuestions(@Param("quizId") Long quizId, @Param("questionType") String questionType);
+    */
+    //correct
+    @Query("select s.question from QuizQuestionJoinTable s where s.quiz.course.id = :courseId and Question_TYPE = :questionType")
+    /*@Query("select question from QuizQuestionJoinTable s where s.quiz.id = :quizId and FUNCTION('CLASS', question) = :questionType")*/
+    <Value extends Question> List<Value> getQuestions(@Param("courseId") Long quizId, @Param("questionType") String questionType);
 
     @Query("select s.quiz from QuizQuestionJoinTable s where s.question.id = :questionId")
     List<Quiz> getQuizzes(@Param("questionId") Long questionId);
 
+    //correct
     @Query("select s.question from QuizQuestionJoinTable s where s.quiz.id = :quizId")
     List<Question> getQuestions(@Param("quizId") Long quizId);
 

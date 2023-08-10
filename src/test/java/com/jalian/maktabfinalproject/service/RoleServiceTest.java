5947effa164a1c1a7eb6336320f8f007dc4ea0aa
amirhosein.jalian <amirhoseinjalian82@gmail.com>
2023-08-11 00:42:05 +0430

    adding RoleServiceTest

---
+++ src/test/java/com/jalian/maktabfinalproject/service/RoleServiceTest.java
@@ -0,0 +1,39 @@
+package com.jalian.maktabfinalproject.service;
+
+import com.jalian.maktabfinalproject.entity.Role;
+import com.jalian.maktabfinalproject.entity.RoleNames;
+import com.jalian.maktabfinalproject.repository.RoleRepository;
+import com.jalian.maktabfinalproject.service.role.RoleService;
+import com.jalian.maktabfinalproject.service.role.RoleServiceImpl;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class RoleServiceTest extends BaseEntityServiceTest<Long, Role, RoleRepository, RoleService> {
+
+    @Override
+    protected RoleService getService() {
+        return new RoleServiceImpl(repository);
+    }
+
+    @Override
+    protected void setup() {
+        value = Role.builder().
+                id(1L).
+                name(RoleNames.STUDENT).build();
+    }
+
+    @Override
+    protected Role newInstance() {
+        return Role.builder().
+                id(2L).
+                name(RoleNames.TEACHER).build();
+    }
+
+    @Override
+    protected Role getNewValueForUpdate() {
+        return Role.builder().
+                id(1L).
+                name(RoleNames.TEACHER).build();
+    }
+}
